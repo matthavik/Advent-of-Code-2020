dafb7ed28f9091592df22e68ab8dfd3213440636
havik <33530829+matthavik@users.noreply.github.com>
2020-12-02 18:18:31 +0000

    Updatteeee

--- Day 1 - Return Product/D1 Python Part 1 and 2.py
+++ Day 1 - Return Product/D1 Python Part 1 and 2.py
@@ -1,7 +1,6 @@
 from itertools import combinations
 from math import prod
 
-# DAY 1
 
 def day1_func(_numbers, _n):
     for vals in combinations(_numbers, _n):
@@ -9,7 +8,7 @@ def day1_func(_numbers, _n):
             return prod(vals)
 
 
-with open("C:\\Python\AdventofCode\Day1\input.txt") as file:
+with open("input.txt") as file:
     numbers = [int(line.rstrip("\n")) for line in file]
 
 print(f"Part 1: {day1_func( numbers, 2)}")
