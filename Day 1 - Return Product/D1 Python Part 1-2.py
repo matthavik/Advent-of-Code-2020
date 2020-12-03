2ce17d541e5d2c9c54a0e6fc44c53ce2d8f2f393
havik <33530829+matthavik@users.noreply.github.com>
2020-12-03 23:55:57 +0000

    Day 3!

--- Day 1 - Return Product/D1 Python Part 1-2.py
+++ Day 1 - Return Product/D1 Python Part 1-2.py
@@ -1,15 +1,15 @@
 from itertools import combinations
 from math import prod
 
-
 def day1_func(_numbers, _n):
     for vals in combinations(_numbers, _n):
         if sum(vals, 0) == 2020:
             return prod(vals)
-
 
 with open("input.txt") as file:
     numbers = [int(line.rstrip("\n")) for line in file]
 
 print(f"Part 1: {day1_func( numbers, 2)}")
 print(f"Part 2: {day1_func( numbers, 3)}")
+
+
---
+++ Day 3 - Toboggan Trajectory/D3 Python Part 1-2.py
@@ -0,0 +1,30 @@
+# Maybe more condensed than it needs to be... but it was fun! Tried two different methods
+
+# Part 1 - using a generator!
+# def mountain_line():
+#     count = -3
+#     for line in open("input.txt"):
+#         count = (count + 3) % (len(line) - 1)
+#         yield line[count]
+
+
+# mountain_gen = mountain_line()
+# trees = sum(char == "#" for char in mountain_gen)
+# print(f"P1 Trees: {trees}")
+
+
+# Part 2 - using ... generator comprehension and for loops!
+with open("input copy.txt") as file:
+    mountain = file.read()
+height = mountain.count("\n") + 1
+mountain = mountain.replace("\n", "")
+width = int(len(mountain) / height)
+
+
+trees, trees_multiple, attempts = 0, 1, [1, 1, 3, 1, 5, 1, 7, 1, 1, 2]
+for i in range(0, len(attempts), 2):
+    for x, y in zip((x % width for x in range(0, len(mountain), attempts[i])), range(0, height, attempts[i + 1])):
+        trees = trees + 1 if mountain[x + width * y] == "#" else trees
+    trees_multiple, trees = trees_multiple * trees, 0
+
+print(f"P2 Trees: {trees_multiple}")
